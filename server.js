d8d9fbef4d91a7340d6d5591a4adddc4e4dba81f
huytd <huytd@nal.vn>
2021-08-17 19:30:18 +0700

    update ports

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ app.get("/", (req, res) => {
   res.send("Hello App1");
 });
 
-// app.get("/about", (req, res) => {
-//   res.send("app1 Audrey app");
-// });
+app.get("/app1", (req, res) => {
+  res.send("app1 Audrey app");
+});
 
-// app.get("/app1/audrey", (req, res) => {
-//   res.send("app1 Audrey app");
-// });
+app.get("/app1/audrey", (req, res) => {
+  res.send("app1 Audrey app");
+});
 
-// app.get("/path1/kube", (req, res) => {
-//   res.send("App1 kubernetes");
-// });
+app.get("/path1/kube", (req, res) => {
+  res.send("App1 kubernetes");
+});
 
 app.listen(PORT);
 console.log(`server is running on port ${PORT}`);
