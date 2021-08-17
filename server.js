7c96c0a5c3574d11aea0557ffe86b4d700a6c599
huytd <huytd@nal.vn>
2021-08-17 20:00:38 +0700

    change text

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const PORT = 8080;
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello App1");
+  res.send("hello Root");
 });
 
 app.get("/app1", (req, res) => {
-  res.send("app1 Audrey app");
+  res.send("hello App1");
 });
 
 app.get("/app1/audrey", (req, res) => {
-  res.send("app1 Audrey app");
+  res.send("hello Audrey");
 });
 
 app.get("/path1/kube", (req, res) => {
