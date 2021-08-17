a6097405e63c97911f796218ae1740bbd41d8ba1
huytd <huytd@nal.vn>
2021-08-17 19:17:36 +0700

    update text

--- server.js
+++ server.js
@@ -5,17 +5,21 @@ const PORT = 8080;
 
 // App
 const app = express();
-app.get("/app1", (req, res) => {
-  res.send("Hello Nodejs Kubernetes");
+app.get("/", (req, res) => {
+  res.send("Hello App1");
 });
 
-app.get("/app1/audrey", (req, res) => {
-  res.send("app1 Audrey app");
-});
+// app.get("/about", (req, res) => {
+//   res.send("app1 Audrey app");
+// });
 
-app.get("/app1/kube", (req, res) => {
-  res.send("App1 kubernetes");
-});
+// app.get("/app1/audrey", (req, res) => {
+//   res.send("app1 Audrey app");
+// });
+
+// app.get("/path1/kube", (req, res) => {
+//   res.send("App1 kubernetes");
+// });
 
 app.listen(PORT);
 console.log(`server is running on port ${PORT}`);
