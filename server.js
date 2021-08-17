8ed5752dea904a9e7e48fb360549632cb716d7b3
huytd <huytd@nal.vn>
2021-08-17 07:28:47 +0700

    update routes

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const PORT = 8080;
 
 // App
 const app = express();
-app.get("/", (req, res) => {
+app.get("/app1", (req, res) => {
   res.send("Hello Nodejs Kubernetes");
 });
 
-app.get("/audrey", (req, res) => {
-  res.send("Audrey app");
+app.get("/app1/audrey", (req, res) => {
+  res.send("app1 Audrey app");
+});
+
+app.get("/app1/kube", (req, res) => {
+  res.send("App1 kubernetes");
 });
 
 app.listen(PORT);
